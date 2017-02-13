7910e6bebdb98161e0eab6c3e8aefba49a8af339
samanthaduarte <Samantha Duarte@Samanta-PC>
2017-02-13 16:53:18 -0600

    Ya funciona la creacion de numeros random y su lectura

--- MergeSort.java
+++ MergeSort.java
@@ -1,14 +1,10 @@
 /**
- *
- */
-
-/**
  * @author Andrea Cordón, 16076
  * @author Cristopher Recinos, 16005
  * @author Samantha Duarte, 16256
  * @author Daniel Rodriguez, 15
  *
  */
-public class RadixSort {
+public class MergeSort {
 
 }
