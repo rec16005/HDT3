bb9474d4593802f6e47123d31cebe69cc186f0d0
andreamcm <cor16076@uvg.edu.gt>
2017-02-13 22:16:35 -0600

    Main
    Convierte el String a un array de int

--- Main.java
+++ Main.java
@@ -1,24 +1,38 @@
 import java.util.*;
 import java.io.*;
 
-//Desarrolle un programa que genere al azar y guarde en un 
-//archivo, números enteros. Debe generarse hasta 3000 números. 
+//Desarrolle un programa que genere al azar y guarde en un
+//archivo, nï¿½meros enteros. Debe generarse hasta 3000 nï¿½meros.
 
-//Luego utilice su programa para leer los datos de ese 
-//archivo, guardarlos en un arreglo y ordenarlos, usando 
+//Luego utilice su programa para leer los datos de ese
+//archivo, guardarlos en un arreglo y ordenarlos, usando
 //cada uno de los algoritmos de sort. Utilice el Profiler
-//para medir el tiempo empleado en el ordenamiento para: 
-//10 números hasta 3000 números. El propósito será 
-//generar una gráfica con el eje Y mostrando el tiempo
-//y el eje X mostrando la cantidad de números ordenados.
-//Usted puede seleccionar los intervalos que considere 
-//adecuados en el eje X. 
+//para medir el tiempo empleado en el ordenamiento para:
+//10 nï¿½meros hasta 3000 nï¿½meros. El propï¿½sito serï¿½
+//generar una grï¿½fica con el eje Y mostrando el tiempo
+//y el eje X mostrando la cantidad de nï¿½meros ordenados.
+//Usted puede seleccionar los intervalos que considere
+//adecuados en el eje X.
 
 public class Main{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	}	
-	
+
+		Archivo random = new Archivo();
+		BufferedReader b = new BufferedReader(new FileReader("numerosRandom.txt"));
+		String linea = b.readLine();
+		String[] array = linea.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\s", "").split(",");
+
+		int[] results = new int[array.length];
+		for (int i = 0; i < array.length; i++) {
+		    try {
+		        results[i] = Integer.parseInt(array[i]);
+		    } catch (NumberFormatException nfe) {
+		        //NOTE: write something here if you need to recover from formatting errors
+		    };
+		}
+
+	}
+
 }
--- RadixSort.java
+++ RadixSort.java
@@ -1,14 +1,34 @@
-/**
- *
- */
 
-/**
- * @author Andrea Cordón, 16076
- * @author Cristopher Recinos, 16005
- * @author Samantha Duarte, 16256
- * @author Daniel Rodriguez, 15
- *
- */
-public class RadixSort {
+// Método extraído de: http://www.sanfoundry.com/java-program-implement-radix-sort/
+public class RadixSort implements Comparable{
+
+	 public static void radixSort( int[] a)
+	    {
+	        int i, m = a[0], exp = 1, n = a.length;
+	        int[] b = new int[10];
+	        for (i = 1; i < n; i++)
+	            if (a[i] > m)
+	                m = a[i];
+	        while (m / exp > 0)
+	        {
+	            int[] bucket = new int[10];
+
+	            for (i = 0; i < n; i++)
+	                bucket[(a[i] / exp) % 10]++;
+	            for (i = 1; i < 10; i++)
+	                bucket[i] += bucket[i - 1];
+	            for (i = n - 1; i >= 0; i--)
+	                b[--bucket[(a[i] / exp) % 10]] = a[i];
+	            for (i = 0; i < n; i++)
+	                a[i] = b[i];
+	            exp *= 10;
+	        }
+	    }
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
